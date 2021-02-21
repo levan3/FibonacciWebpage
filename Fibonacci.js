6a0c8fbf4ae339aadbb56454a89354fc214f2e78
levan3 <74518641+levan3@users.noreply.github.com>
2021-02-21 15:25:30 +1300

    Declare JS variable
    Declare JS variable

---
+++ Fibonacci.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("reveal-button");
+var position = document.getElementById("position");
+var answer = document.getElementById("answer");
+
+var result = fibonacci(position);
