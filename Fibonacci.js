3b41ad986a346fe0fb3ba4f6500dd9f0d31906cf
levan3 <74518641+levan3@users.noreply.github.com>
2021-02-21 16:18:45 +1300

    JS logic working
    Need to investigate time-space complexity

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,22 +1,32 @@
-var button = document.getElementById("reveal-button");
-var fibonacciform = document.getElementById("fibonacciform");
 
-fibonacciform.onsubmit = function() {
-    return false;
-}
+	var button = document.getElementById("reveal");
+	
+	var form = document.getElementById("fibonacciform");
+	form.onsubmit = function() {
+		return false;
+	}
 
-button.onclick = function() {
-var position = document.getElementById("position");
-var answer = document.getElementById("answer");
+	button.onclick = function() {
+    	
+		//declare var input for fibonacci position
+		var position = document.getElementById("position").value;
+		//declare var output for fibonacci position
+		var answer = document.getElementById("answer");
+		var answer2 = document.getElementById("answer2");
+		//declare var result
+		var result = fibonacci(position);
+		
+		
+		//result return
+		answer.innerHTML = "<p>  Result is </p>";
+   		answer2.innerHTML = result;
 
-var result = fibonacci(position);
 
-answer.innerHTML = result;
-}
+	}
 
-function fibonacci(n){
-    {
-		if ( n < 1 ) { return "position must not be a negative number";
+
+	function fibonacci(n) {
+		if ( n < 1 ) { return "Input must be a number greater than 0.";
 		} else if ( n == 1 ) {
 			return 0; 
 		} else if ( n < 3 ) {
@@ -25,4 +35,5 @@ function fibonacci(n){
 				return fibonacci(n-1)+fibonacci(n-2);
 		}
 	}
-}
+	
+  
